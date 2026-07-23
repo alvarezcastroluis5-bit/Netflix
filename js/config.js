a951f802f353da86181413d70fd64fe4ee601299
alvarezcastroluis5-bit <alvarezcastroluis5@gmail.com>
2026-07-22 21:14:55 -0400

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,4 @@
+export const SUPABASE_URL = "https://rmligdfmfwpmdsllembk.supabase.co";
+export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_Ld1G5Si_wWzHK_-G-ZUBlg_KQOFO2Ov";
+export const USER_MANAGER_FUNCTION = "hyper-processor";
+export const NETFLIX_EXTERNAL_URL = "";
